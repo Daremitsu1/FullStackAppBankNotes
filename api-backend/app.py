ee97230b4999ea65c085c6a427bee200609a63f0
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2024-02-14 23:57:38 +0530

    Push files

--- api-backend/app.py
+++ api-backend/app.py
@@ -1,21 +1,34 @@
-# Bring in our dependencies
 from fastapi import FastAPI
 from pydantic import BaseModel
-import requests, pickle
+import pickle
+import numpy as np
+
+app = FastAPI()
 
 # Load the pickle file
 with open('D:/Python Projects/FullStackAppBankNotes/models/classifier.pkl', 'rb') as f:
     classifier = pickle.load(f)
 
-app = FastAPI()
+# Defining class for model predictions
+class ScoringItem(BaseModel):
+    variance: float
+    skewness: float
+    curtosis: float
+    entropy: float
 
-class item(BaseModel):
-    variance = requests.args.get('variance')
-    skewness = requests.args.get('skewness')
-    curtosis = requests.args.get('curtosis')
-    entropy = requests.args.get('entropy')
+# Define the POST endpoint
+@app.post("/predict")
+async def predict(item: ScoringItem):
+    # Extracting values from the request body
+    variance = item.variance
+    skewness = item.skewness
+    curtosis = item.curtosis
+    entropy = item.entropy
+    
+    # Making prediction using the classifier
     prediction = classifier.predict([[variance, skewness, curtosis, entropy]])
-
-@app.get("/")
-def welcome():
-    return "Hello World!"
+    
+    # Convert numpy.int64 to int
+    prediction = prediction.item()
+    
+    return {"prediction": prediction}
