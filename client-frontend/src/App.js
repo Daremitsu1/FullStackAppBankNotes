f1231056a803a83f67db4b6e0bbf658e965a6e1d
Aviparna Biswas <54842807+Daremitsu1@users.noreply.github.com>
2024-02-15 15:35:31 +0530

    Push frontend files

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,15 +1,94 @@
-import React from 'react';
-import "./app.scss"
+import React, { useState } from 'react';
+import "./app.scss";
+import axios from "axios";
+import {
+  Form,
+  FormGroup,
+  TextInput,
+  Button,
+  Loading
+} from "carbon-components-react";
 
 function App() {
+  const [varianceValue, setVarianceValue] = useState('');
+  const [skewnessValue, setSkewnessValue] = useState('');
+  const [curtosisValue, setCurtosisValue] = useState('');
+  const [entropyValue, setEntropyValue] = useState('');
+  const [predictions, setPredictions] = useState(null); // State variable for predictions
+  const [loading, setLoading] = useState(false); // State variable for loading state
+
+  const runPred = async () => {
+    try {
+      setLoading(true); // Set loading state to true while fetching prediction
+      const response = await axios.post("/predict", {
+        variance: parseFloat(varianceValue),
+        skewness: parseFloat(skewnessValue),
+        curtosis: parseFloat(curtosisValue),
+        entropy: parseFloat(entropyValue)
+      });
+      setPredictions(response.data.prediction); // Update predictions state with prediction data
+      console.log("Prediction:", response.data);
+    } catch (error) {
+      console.error("Error predicting:", error);
+    } finally {
+      setLoading(false); // Set loading state back to false after fetching prediction
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-        
-      </header>
+      <div className='mainContainer'>
+        <Form>
+          <FormGroup>
+            <TextInput 
+              id="variance-input" 
+              labelText="Enter Variance"
+              onChange={(e) => setVarianceValue(e.target.value)}
+              value={varianceValue}
+            />
+          </FormGroup>
+          <FormGroup>
+            <TextInput 
+              id="skewness-input" 
+              labelText="Enter Skewness"
+              onChange={(e) => setSkewnessValue(e.target.value)}
+              value={skewnessValue}
+            />
+          </FormGroup>
+          <FormGroup>
+            <TextInput 
+              id="curtosis-input" 
+              labelText="Enter Curtosis"
+              onChange={(e) => setCurtosisValue(e.target.value)}
+              value={curtosisValue}
+            />
+          </FormGroup>
+          <FormGroup>
+            <TextInput 
+              id="entropy-input" 
+              labelText="Enter Entropy"
+              onChange={(e) => setEntropyValue(e.target.value)}
+              value={entropyValue}
+            />
+          </FormGroup>
+          <FormGroup>
+            <Button onClick={runPred}>
+              Predict
+            </Button>
+          </FormGroup>
+        </Form>
+        <div className='predictContainer'>
+          {loading && <Loading description='Loading...' />} {/* Display loading indicator if loading state is true */}
+          {predictions !== null && !loading && (
+            <div>
+              The model predicted:
+              <div className='predictionResult'>
+                {parseFloat(predictions)}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client-frontend/src/utils.js
@@ -0,0 +1,5 @@
+// Exporting variables representing features of bank notes
+export const variance = '';
+export const skewness = '';
+export const curtosis = '';
+export const entropy = '';
